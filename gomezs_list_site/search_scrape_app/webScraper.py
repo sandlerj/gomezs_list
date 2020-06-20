820015b47d40bda20f3aca9673ab962fc843cacd
sandlerj <sandlerjoseph@yahoo.com>
2020-06-20 00:30:50 -0400

    webScaper waiting for nearestCity to be implemented

--- gomezs_list_site/search_scrape_app/webScraper.py
+++ gomezs_list_site/search_scrape_app/webScraper.py
@@ -6,14 +6,14 @@ GOOGLE_SECRET = os.getenv("GOOGLE_SECRET")
 '''
 This is going to need a lot of testing you twit
 '''
-def scrapeCraigsList(searchQuery, where, cat="sss"):
+def scrapeCraigsList(search_query, where, cat="sss"):
     """
     Creates an HTMLSession so as to execute a few clicks on
     the page before rendering. This is to ensure that the
     <img> elements are actually showing as craigslist 
     usually initializes with list view w/o images
     """
-    city = nearestCity(where)
+    city_subdomain = nearestCitySubdomain(where)
     session = HTMLSession()
     
     #  Click the gridview button twice - once to open drop down and
@@ -21,7 +21,7 @@ def scrapeCraigsList(searchQuery, where, cat="sss"):
     script="document.querySelector(\"#gridview\").click();" +
     "document.querySelector(\"#gridview\").click();"
     
-    r = session.get(formatQuery(city, searchQuery, cat))
+    r = session.get(formatQuery(city_subdomain, search_query, cat))
     r.html.render(script=script)
     content = r.html.find("div#sortable-results ul.rows", first=True).html
     resultsRows = BeautifulSoup(content,"html.parser")
@@ -55,16 +55,34 @@ def scrapeCraigsList(searchQuery, where, cat="sss"):
         tmpDict["price"] = price
 
         li_dicts.append(tmpDict)
-    retun li_dicts
 
-def formatQuery(city,searchQuery, catergory):
+    return li_dicts
+
+def formatQuery(city_subdomain,search_query, catergory):
 """
 Formats the search query and any other params following the pattern used in 
 GET requests to /search/
 """
-    return
+    url = city_subdomain + "/search/" + category + "?query=" + search_query
+    return url
 
 def nearestCity(location):
     # Given a location, returns the nearest city which has its own craigslist
     # subdomain
-    return
+    subdomain_dict = loadSubdomainDict()
+    
+    # Use google places to check where "location" is
+
+    # Get country state and probably city/metro area of "location"
+
+    # Get lowest subtree possible
+        
+        #using these limited results, use Distance Matrix to see which is close
+    
+    return #closest_subdomain
+
+def loadSubdomainDict():
+    with open("cl_subdomains" + os.sep + "cl_subdomains.json", "r") as f:
+        jsontxt = f.read()
+    subdomain_dict = json.loads(jsontxt)
+    return subdomain_dict
